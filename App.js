7ec54d38c230f76d4a14e832a98e4dd93de17b9a
Admin <admin@MacBook-Pro-cua-Admin.local>
2022-12-22 23:38:00 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { FlatList, View } from "react-native";
+import styled from "styled-components";
+import AddInput from "./component/AddInput";
+import TodoList from "./component/TodoList";
+import Header from "./component/Header";
+import Empty from "./component/Empty"
 
 export default function App() {
+  const [data, setData] = useState([]);
+
+  const submitHandler = (value) => {
+    setData((prevTodo) => {
+      return [
+        {
+          value: value,
+          key: Math.random().toString(),
+        },
+        ...prevTodo,
+      ];
+    });
+  };
+
+  const deleteItem = (key) => {
+    setData((prevTodo) => {
+      return prevTodo.filter((todo) => todo.key != key);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ComponentContainer>
+      <View>
+        <StatusBar barStyle="light-content" backgroundColor="midnightblue" />
+      </View>
+
+      <View>
+        <FlatList
+          data={data}
+          ListHeaderComponent={() => <Header />}
+          ListEmptyComponent={() => <Empty />}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => (
+            <TodoList item={item} deleteItem={deleteItem} />
+          )}
+        />
+        <AddInput submitHandler={submitHandler} />
+      </View>
+    </ComponentContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const ComponentContainer = styled.View`
+  background-color: midnightblue;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
